78686b566f97675f606e6b3fd099f8cb045d6730
Ula Rrr <uromaniec@gmail.com>
2017-04-23 18:24:45 +0200

    ok

---
+++ src/model/HibernateUtil.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author Ulka
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    
+    public static List executeHQLListQuery(Session session, String hql) {
+        List resultList = null;
+        try {
+            //Session session = getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.createQuery(hql);
+            resultList = q.list();
+            session.getTransaction().commit();
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return resultList;
+    }
+    
+    public static int executeHQLUniqueQuery(Session session, String hql) {
+        int result = 0;
+        try {
+            //Session session = getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.createQuery(hql);
+            result = (int) q.uniqueResult();
+            session.getTransaction().commit();
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return result;
+    }
+    
+    public static void executeHQLsave(Session session, Object o) {
+        try {
+            //Session session = getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(o);
+            session.getTransaction().commit();
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+    }
+}
---
+++ src/tcp_server/SocketServer.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcp_server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import model.PomiarTemperatura;
+import model.ProducentPomiarowNew;
+import view.UrzadzeniaForm1;
+
+/**
+ *
+ * @author Ulka
+ */
+public class SocketServer extends Thread {
+
+    private ServerSocket serverSocket;
+    private final ProducentPomiarowNew producent;
+    private boolean running = true;
+
+    public SocketServer(int port, boolean running) throws IOException {
+        serverSocket = new ServerSocket(port);
+        //serverSocket.setSoTimeout(10000);
+        this.running = running;
+        producent = new ProducentPomiarowNew();
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                System.out.println("Waiting for client on port "
+                        + serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+
+                System.out.println("Just connected to " + server.getRemoteSocketAddress());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+
+                String read = in.readUTF();
+                System.out.println(read);
+                readData(read);
+
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                out.writeUTF("Thank you for sending info to " + server.getLocalSocketAddress());
+                //server.close();
+
+            } catch (SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    private void readData(String readUTF) {
+        //odczyt temperatury
+        System.out.println("Server wczytuje zbiór danych pomiarowych...");
+        String[] zbior = readUTF.split("#");
+        for (int i = 0; i < zbior.length; i++) {
+            System.out.println("Wczytuję pomiar " + i + "...");
+            String[] pomiar = zbior[i].split(";");
+            int id = Integer.parseInt(pomiar[1]);
+            if (zbior[i].startsWith("T")) {
+                BigDecimal wynik = new BigDecimal(pomiar[2]);
+                producent.newPomiarTemp(id, wynik);
+            } else if (zbior[i].startsWith("G")){
+                BigDecimal wynikNap = new BigDecimal(pomiar[2]);
+                BigDecimal wynikPrad = new BigDecimal(pomiar[3]);
+                BigDecimal wynikMoc = new BigDecimal(pomiar[4]);
+            }
+            else {
+                System.out.println("Nieprawidłowy zapis pomiaru");
+            }
+        }
+
+        //odczyt gniazdek
+    }
+}
